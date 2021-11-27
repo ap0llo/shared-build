9e0084de9b51716e40ea8fe7dd8c332aa016fafb
Andreas Grünwald <andreas@grynwald.net>
2021-11-27 17:00:18 +0100

    build: Reference tools using a .NET local tool manifest

--- build/Program.cs
+++ build/Program.cs
@@ -1,15 +1,15 @@
 ﻿using System;
 using Cake.AzurePipelines.Module;
 using Cake.Core;
+using Cake.DotNetLocalTools.Module;
 using Cake.Frosting;
 using Grynwald.SharedBuild;
 
 return new CakeHost()
     .UseContext<BuildContext>()
     .UseModule<AzurePipelinesModule>()
-    //TODO: Use Tool manifest
-    .InstallTool(new Uri("dotnet:?package=Grynwald.ChangeLog&version=0.4.135"))
-    .InstallTool(new Uri("dotnet:?package=dotnet-reportgenerator-globaltool&version=5.0.0"))
+    .UseModule<LocalToolsModule>()
+    .InstallToolsFromManifest(".config/dotnet-tools.json")
     .Run(args);
 
 
