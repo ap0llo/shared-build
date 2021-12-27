51312fcc30cfcabf57c698eaba3f9faa6780ef78
Andreas Grünwald <andreas@grynwald.net>
2021-12-27 17:54:40 +0100

    feat: Add factory methods for creating a 'IPushTarget' for nuget.org
    Add KnownPushTargets.NuGetOrg() factory method for creating an 'IPushTarget' for nuget.org so the feed url for nuget.org does not have to be repeated in every build project

---
+++ src/SharedBuild/PushTargets.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Grynwald.SharedBuild
+{
+    public static class PushTargets
+    {
+        public static IPushTarget NuGetOrg(Func<IBuildContext, bool> isActive) => new PushTarget(
+            type: PushTargetType.NuGetOrg,
+            feedUrl: "https://api.nuget.org/v3/index.json",
+            isActive: isActive
+        );
+    }
+}
