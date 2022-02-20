6135578a8b0f59a04d67f24fb5bdf8535eccb828
Andreas Grünwald <andreas@grynwald.net>
2022-02-20 13:53:35 +0100

    build: Remove installation of .NET 5 runtime
    The .NET 5 runtime was required for running the "changelog" tools.
    Since upgrading to version 1.0 which has .NET 6 support, installign the .NET runtime is no longer required.

--- build.ps1
+++ build.ps1
@@ -4,8 +4,6 @@ if ($env:TF_BUILD) {
     Write-Host "##[group]Install .NET SDK"
 }
 
-./build/dotnet-install.ps1 -Channel 5.0 -Runtime dotnet
-
 # Install SDK and runtime as specified in global.json
 ./build/dotnet-install.ps1 -JsonFile "$PSScriptRoot/global.json"
 
