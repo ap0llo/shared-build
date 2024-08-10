4bf868316b566084779ad213e2eac2c1aec0586a
Andreas Grünwald <andreas@grynwald.net>
2024-08-10 23:02:28 +0200

    Retarget all proejct to .NET 8

--- build.ps1
+++ build.ps1
@@ -7,9 +7,6 @@ if ($env:TF_BUILD) {
 # Install SDK and runtime as specified in global.json
 ./build/dotnet-install.ps1 -JsonFile "$PSScriptRoot/global.json"
 
-# Install .NET 6 runtime (required for running tests)
-./build/dotnet-install.ps1 -Channel 6.0 -Runtime dotnet
-
 Invoke-Expression "dotnet --info"
 
 if ($env:TF_BUILD) {
