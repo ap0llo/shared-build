199c9aa48e60d7d103457af8d1172053645208b7
Andreas Grünwald <andreas@grynwald.net>
2024-12-21 13:17:12 +0100

    Add bash version of the build script to run the build on Linux

--- build.sh
+++ build.sh
@@ -1,1 +1,23 @@
-dotnet run --project ./build/Build.csproj -- "$@"
+#!/bin/bash
+
+# Define variables (that might or might not be set)
+# Without this, the script will fail with an unbound variable" error (because of the 'set -e')
+TF_BUILD=$TF_BUILD
+
+set -e
+
+if [[ -n "${TF_BUILD}" ]]; then
+    echo "##[group]Install .NET SDK"
+fi
+
+# Install SDK and runtime as specified in global.json
+source ./build/dotnet-install.sh --jsonfile "global.json"
+
+dotnet --info
+
+
+if [[ -n "${TF_BUILD}" ]]; then
+    echo "##[endgroup]"
+fi
+
+dotnet run --project build/Build.csproj -- $@
