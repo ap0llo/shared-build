3c76da116ad461edcc81782a907b523f5a624731
Andreas Grünwald <andreas@grynwald.net>
2024-12-21 13:48:48 +0100

    Add "GitHubActions" to build context and adapt DefaultBuildContext.IsRunningInCI to return true if build is running either on Azure Pipelines or GitHub actions

---
+++ src/SharedBuild/_Context/_Default/DefaultGitHubActionsContext.cs
@@ -0,0 +1,18 @@
+﻿using Cake.Common.Build;
+using Cake.Common.Build.GitHubActions;
+using Cake.Core.Diagnostics;
+
+namespace Grynwald.SharedBuild;
+
+public class DefaultGitHubActionsContext(DefaultBuildContext context) : IGitHubActionsContext
+{
+    private readonly IGitHubActionsProvider m_GitHubActionsProvider = context.GitHubActions();
+
+    public bool IsActive => m_GitHubActionsProvider.IsRunningOnGitHubActions;
+
+
+    public void PrintToLog(ICakeLog log)
+    {
+        log.Information($"{nameof(IsActive)}: {IsActive}");
+    }
+}
---
+++ src/SharedBuild/_Context/_GitHubUrlParser/IGitHubActionsContext.cs
@@ -0,0 +1,9 @@
+﻿namespace Grynwald.SharedBuild;
+
+public interface IGitHubActionsContext : IPrintableObject
+{
+    /// <summary>
+    /// Gets whether the build is currently running in GitHub Actions
+    /// </summary>
+    bool IsActive { get; }
+}
