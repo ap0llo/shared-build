844761fcc53943bdd825900d56abd6dcf7740dee
Andreas Grünwald <andreas@grynwald.net>
2024-12-21 14:55:37 +0100

    Group log output of .NET SDK installation on GitHub Actions

--- build.sh
+++ build.sh
@@ -3,11 +3,16 @@
 # Define variables (that might or might not be set)
 # Without this, the script will fail with an unbound variable" error (because of the 'set -e')
 TF_BUILD=$TF_BUILD
+GITHUB_ACTIONS=$GITHUB_ACTIONS
 
 set -e
 
+# Group log output of SDK installation on GitHub Actions or Azure Pipelines
 if [[ -n "${TF_BUILD}" ]]; then
     echo "##[group]Install .NET SDK"
+fi
+if [[ "${GITHUB_ACTIONS}" == "true" ]]; then
+    echo "::group::Install .NET SDK"
 fi
 
 # Install SDK and runtime as specified in global.json
@@ -16,8 +21,12 @@ source ./build/dotnet-install.sh --jsonfile "global.json"
 dotnet --info
 
 
+# End grouping of log output on GitHub Actions or Azure Pipelines
 if [[ -n "${TF_BUILD}" ]]; then
     echo "##[endgroup]"
+fi
+if [[ "${GITHUB_ACTIONS}" == "true" ]]; then
+    echo "::endgroup::"
 fi
 
 dotnet run --project build/Build.csproj -- $@
